f6fac85828f5a28b6907748bac1088e2ba4aa51f
SSR707 <samandarshavkatov07@gmail.com>
2025-03-29 15:16:37 +0500

    website in prograss

--- src/app/_components/home/info/info-card.tsx
+++ src/app/_components/home/info/info-card.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import NextIcon from "../../../../../public/svg/next-icon.svg";
 export const InfoCard = () => {
   return (
     <div className=" text-center p-[20px]  shadow-2xl mb-[30px] lg:mb-0 ">
@@ -8,17 +7,22 @@ export const InfoCard = () => {
       </h2>
 
       <div
-        className={`px-[15px] py-[10px] md:w-[300px] rounded-2xl bg-[#000] flex gap-2 md:gap-2.5 items-center border-1
+        className={`px-[13px] py-[10px] md:w-[320px] rounded-2xl bg-[#000] flex gap-[5px] md:gap-2.5 items-center border-1
           shadow-[0px_0px_20px_1px_rgba(255,255,255,0.263)]
            border-gray-300 mr-auto ml-auto motion-safe:animate-[bounce_1.5s_infinite]
            hover:shadow-[0px_0px_10px_2px_rgba(255,255,255,0.50)] hover:scale-105 transition-all duration-400`}
       >
-        <div className="w-2 h-2 bg-white rounded-full animate-ping"></div>
+        <div className="w-2 h-2 bg-white rounded-full shadow-[0px_0px_10px_2px_rgba(255,255,255,0.50)] animate-pulse"></div>
         <span className="text-gray-400 font-bold text-[14px]">New EP</span>{" "}
         <span className="text-[#fff] font-bold drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.263)]  text-[14px]">
           "KOZLARIM"
         </span>
         <span className="text-gray-500 font-bold text-[13px]">Coming Soon</span>
+        <img
+          src={NextIcon.src}
+          className="w-[16px] h-[16px] md:w-[20px] md:h-[20px] text-white"
+          alt=""
+        />
       </div>
     </div>
   );
