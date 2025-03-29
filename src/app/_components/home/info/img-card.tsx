0287c76b989977f8502a36503596e5adc46a1d23
SSR707 <samandarshavkatov07@gmail.com>
2025-03-29 20:58:09 +0500

    website in prograss

--- src/app/_components/home/info/img-card.tsx
+++ src/app/_components/home/info/img-card.tsx
@@ -1,6 +1,6 @@
 export const ImgCard = ({ img }: { img: string }) => {
   return (
-    <div className="h-[420px] w-full lg:w-[400px]  md:h-[600px] ">
+    <div className="h-[435px] w-full lg:w-[400px]  md:h-[600px] ">
       <img
         className="w-full h-full object-cover rounded-[15px] "
         src={img}
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,44 +5,56 @@ import YandexIcon from "../../../public/svg/yandex.svg";
 import AppleMusiccon from "../../../public/svg/apple-music.svg";
 const Footer = () => {
   return (
-    <footer className="pt-[30px] pb-[5px] bg-foreground shadow-[0px_0px_10px_3px_rgba(255,255,255,0.5)]">
+    <footer className="pt-[30px] pb-[5px] md:pt-[50px] md:pb-[10px] bg-foreground shadow-[0px_0px_10px_3px_rgba(255,255,255,0.5)]">
       <div className="container">
-        <div className="flex flex-col items-center gap-[20px]">
-          <div className="flex gap-[15px]">
-            <div className="w-[32px] h-[32px]  hover:drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.3)] hover:scale-120 transition-all duration-300">
+        <div className="flex flex-col items-center gap-[20px] md:gap-[30px]">
+          <div className="flex gap-[15px] md:gap-5">
+            <div className="w-[32px] h-[32px] md:w-[36px] md:h-[36px]  hover:drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.3)] hover:scale-120 transition-all duration-300">
               {" "}
               <a href="https://www.instagram.com/ibraloveelyy?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
                 <img
-                  className="w-full h-full "
+                  className="w-full h-full object-cover"
                   src={InstagramIcon.src}
                   alt=""
                 />
               </a>
             </div>
-            <div className="w-[32px] h-[32px]">
+            <div className="w-[32px] h-[32px]  md:w-[36px] md:h-[36px]  hover:drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.3)] hover:scale-120 transition-all duration-300 ">
               {" "}
               <a href="https://open.spotify.com/album/55J50LmmZMZ7nbLMsliSud?si=P5SC-2ZrSvG-weqfqV3Gmg">
                 <img
-                  className="w-full h-full hover:drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.3)] hover:scale-120 transition-all duration-300"
+                  className="w-full h-full object-cover"
                   src={SpotifyIcon.src}
                   alt=""
                 />
               </a>
             </div>
-            <div className="w-[32px] h-[32px] hover:drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.3)] hover:scale-120 transition-all duration-300">
+            <div className="w-[32px] h-[32px]   md:w-[36px] md:h-[36px]  hover:drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.3)] hover:scale-120 transition-all duration-300">
               <a href="https://youtube.com/@ibraonelove-?si=KSyeXsazPHdq2Euf">
-                <img className="w-full h-full" src={YoutubeIcon.src} alt="" />
+                <img
+                  className="w-full h-full object-cover"
+                  src={YoutubeIcon.src}
+                  alt=""
+                />
               </a>
             </div>
-            <div className="w-[32px] h-[32px] hover:drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.3)] hover:scale-120 transition-all duration-300">
+            <div className="w-[32px] h-[32px]   md:w-[36px] md:h-[36px]  hover:drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.3)] hover:scale-120 transition-all duration-300">
               <a href="https://music.apple.com/ru/artist/ibra-one-love/1702993085">
-                <img className="w-full h-full" src={AppleMusiccon.src} alt="" />
+                <img
+                  className="w-full h-full object-cover"
+                  src={AppleMusiccon.src}
+                  alt=""
+                />
               </a>
             </div>
-            <div className="w-[32px] h-[32px] hover:drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.3)] hover:scale-120 transition-all duration-300">
+            <div className="w-[32px] h-[32px]   md:w-[36px] md:h-[36px]  hover:drop-shadow-[0px_0px_4.2px_rgba(255,255,255,0.3)] hover:scale-120 transition-all duration-300">
               {" "}
               <a href="https://music.yandex.uz/artist/19910464?utm_source=web&utm_medium=copy_link">
-                <img className="w-full h-full" src={YandexIcon.src} alt="" />
+                <img
+                  className="w-full h-full object-cover"
+                  src={YandexIcon.src}
+                  alt=""
+                />
               </a>
             </div>
           </div>
