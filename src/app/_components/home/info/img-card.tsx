ac8b911b45efce3e7be966c23cbd3bd2e68f4514
SSR707 <samandarshavkatov07@gmail.com>
2025-03-30 21:15:04 +0500

    website in prograss

--- src/app/_components/home/info/img-card.tsx
+++ src/app/_components/home/info/img-card.tsx
@@ -1,6 +1,6 @@
 export const ImgCard = ({ img }: { img: string }) => {
   return (
-    <div className="h-[500px] w-full lg:w-[400px]  md:h-[600px] ">
+    <div className="h-[470px] w-full lg:w-[400px]  md:h-[600px] ">
       <img
         className="w-full h-full object-cover rounded-[15px] "
         src={img}
